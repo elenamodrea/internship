36bb69317d248f503e8838dc8960f64e6bc07fc1
Elena Modrea <emodrea84@gmail.com>
2024-11-11 13:57:26 +0200

    Modified code using class in python

--- main.py
+++ main.py
@@ -1,26 +1,11 @@
-from youtube_usage import initialize, use_youtube, search_video, click_video
-from log import message_logger
+from youtube_usage import YoutubeUsage
+from youtube_logger import YoutubeLogger
 import time
 
 
 def main():
-    driver = initialize()
-    try:
-        use_youtube(driver)
-
-        # Get search query from the user
-        query = input("Enter youtube title: ")
-        message_logger(f"Entered youtube query: {query}", "info")
-        search_video(driver, query)
-        click_video(driver)
-        time.sleep(5)
-
-    except Exception as e:
-        message_logger(f"Unexpected error: {e}", 'error')
-
-    finally:
-        driver.quit()
-        message_logger("Ended the session", 'info')
+    youtube_usage = YoutubeUsage()
+    youtube_usage.usage()
 
 
 if __name__ == "__main__":
--- youtube_usage.py
+++ youtube_usage.py
@@ -1,4 +1,7 @@
+import sys
+
 from selenium import webdriver
+from selenium.common import WebDriverException
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
@@ -6,65 +9,106 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as ec
-from log import message_logger
+from youtube_logger import YoutubeLogger
 import time
+import requests
 
 
-def initialize():
-    # Set Chrome options to start in full-screen mode
-    chrome_options = Options()
-    chrome_options.add_argument("--start-fullscreen")
+class YoutubeUsage:
+    def __init__(self):
+        self.youtube_logger = YoutubeLogger(file_path="log_script.log", file_name="log_script.log")
+        # Set Chrome options to start in full-screen mode
+        chrome_options = Options()
+        chrome_options.add_argument("--start-fullscreen")
 
-    # Configure Chrome drive
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=chrome_options)
-    message_logger("Chrome drive configured", "info")
-    return driver
+        # self.check_network()
+        # Configure Chrome drive
+        try:
+            service = Service(ChromeDriverManager().install())
+            self.driver = webdriver.Chrome(service=service, options=chrome_options)
+            self.youtube_logger.log_message(self.youtube_logger.Level.INFO.value, "Chrome driver configured")
+        except WebDriverException as e:
+            self.youtube_logger.log_message(self.youtube_logger.Level.ERROR.value, f"Error during driver setup: {e}")
+            sys.exit(1)
+        except Exception as e:
+            self.youtube_logger.log_message(self.youtube_logger.Level.ERROR.value, f"Unexpected error: {e}")
+            sys.exit(1)
 
+    def check_network(self):
+        try:
+            requests.head(url="https://www.google.com", timeout=5)
+            self.youtube_logger.log_message(self.youtube_logger.Level.INFO.value, "Network connection is available.")
+        except requests.ConnectionError:
+            self.youtube_logger.log_message(self.youtube_logger.Level.ERROR.value, "Network connection is not "
+                                                                                   "available.")
+            sys.exit(1)
 
-def use_youtube(driver):
-    # Launch YouTube
-    driver.get("https://www.youtube.com")
-    time.sleep(5)
-    message_logger("Launched Youtube", "info")
-    # Close cookies pop-up
-    try:
-        accept_button = WebDriverWait(driver, 5).until(
-            ec.element_to_be_clickable((By.XPATH, "//button[.//span[contains(text(), 'Accept all')]]"))
+    def use_youtube(self):
+        # Launch YouTube
+        try:
+            self.driver.get("https://www.youtube.com")
+            time.sleep(5)
+            page_state = self.driver.execute_script('return document.readyState;')
+            if page_state == 'complete':
+                self.youtube_logger.log_message(self.youtube_logger.Level.INFO.value, "Launched Youtube")
+                # Close cookies pop-up
+                try:
+                    accept_button = WebDriverWait(self.driver, 5).until(
+                        ec.element_to_be_clickable((By.XPATH, "//button[.//span[contains(text(), 'Accept all')]]"))
+                    )
+                    accept_button.click()
+                    self.youtube_logger.log_message(self.youtube_logger.Level.INFO.value, "Clicked on accept all "
+                                                                                          "button to close the "
+                                                                                          "cookies pop-up")
+                    time.sleep(3)
+                except Exception as e:
+                    self.youtube_logger.log_message(self.youtube_logger.Level.ERROR.value,
+                                                    f"Could not find or click 'Accept All' button: {e}")
+            else:
+                self.youtube_logger.log_message(self.youtube_logger.Level.WARNING.value,
+                                                "YouTube page wasn't loaded in 5 "
+                                                "seconds")
+                sys.exit(1)
+        except WebDriverException as e:
+            self.youtube_logger.log_message(self.youtube_logger.Level.ERROR.value,
+                                            f"Error: Network is unavailable. Browser cannot load the page: {e}")
+            self.driver.quit()
+        except Exception as e:
+            self.youtube_logger.log_message(self.youtube_logger.Level.ERROR.value,
+                                            f"Unexpected error: {e}")
+            self.driver.quit()
+
+    def search_video(self):
+        query: str = "Selenium"
+        # Search a video
+        search_box = self.driver.find_element(By.NAME, "search_query")
+        search_box.send_keys(query)
+        search_box.send_keys(Keys.RETURN)
+
+        self.youtube_logger.log_message(self.youtube_logger.Level.INFO.value,
+                                        f"Searching video with title: {query}")
+
+        # Wait for videos result
+        WebDriverWait(self.driver, 10).until(
+            ec.presence_of_element_located((By.ID, "video-title"))
         )
-        accept_button.click()
-        message_logger("Clicked on accept all button to close the cookies pop-up", "info")
-    except Exception as e:
-        message_logger(f"Could not find or click 'Accept All' button: {e}", "error")
+        self.youtube_logger.log_message(self.youtube_logger.Level.INFO.value,
+                                        "Waiting for video results")
 
-    # Wait for cookies to be visible
-    time.sleep(3)
+    def click_video(self):
+        # Click on video
+        first_video = self.driver.find_element(By.ID, "video-title")
+        first_video.click()
+        self.youtube_logger.log_message(self.youtube_logger.Level.INFO.value,
+                                        "Clicked on the first video")
 
+        # Wait for the video
+        self.youtube_logger.log_message(self.youtube_logger.Level.INFO.value,
+                                        "Wait for the video to be played")
+        time.sleep(120)
 
-def search_video(driver, query):
-    # Search a video
-    search_box = driver.find_element(By.NAME, "search_query")
-    search_box.send_keys(query)
-    search_box.send_keys(Keys.RETURN)
-
-    message_logger(f"Searching video with title: {query}", "info")
-
-    # Wait for videos result
-    WebDriverWait(driver, 10).until(
-        ec.presence_of_element_located((By.ID, "video-title"))
-    )
-
-    message_logger("Waiting for video results", "info")
-
-
-def click_video(driver):
-    # Click on video
-    first_video = driver.find_element(By.ID, "video-title")
-    first_video.click()
-    message_logger("Clicked on the first video", "info")
-
-    message_logger("Clicked on the first video", "info")
-
-    # Wait for the video
-    message_logger("Wait for the video to be played", "info")
-    time.sleep(120)
+    def usage(self):
+        self.use_youtube()
+        self.search_video()
+        self.click_video()
+        self.driver.close()
